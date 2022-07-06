5fee1d3d6b64f6b726d07a7d090e594801af1a15
bouakaz42 <bouakaz42@gmail.com>
2022-07-06 17:33:26 +0100

    fix url looks

--- bio/views.py
+++ bio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 from .models import Bio
 from .serializers import BioSerializer
 from rest_framework import generics
@@ -7,6 +7,11 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.decorators import api_view  , permission_classes
 from rest_framework.response import Response
 from .serializers import ContentSerializer , SociallinksSerializer
+
+
+
+
+
 class BioList(generics.ListCreateAPIView):
     queryset = Bio.objects.all()
     serializer_class = BioSerializer
@@ -31,6 +36,9 @@ class BioDetails(generics.RetrieveUpdateDestroyAPIView):
         serializer.save(user=self.request.user)
     def perform_destroy(self, instance):
         instance.delete()
+
+
+
 # Create your views here.
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -40,6 +48,7 @@ def biolink(request , page_alias):
         content = ContentSerializer(final_page.content)
         social_links = SociallinksSerializer(final_page.social_links)
         
-        final_data = {'name': final_page.page_name,  'page_alias': final_page.page_alias ,'content': content.data , 'social_links': social_links.data}
+        final_data = {'name': final_page.page_name,  'page_alias': final_page.page_alias ,
+                        'content': content.data , 'social_links': social_links.data}
         return Response(final_data , status=201)
     return Response(data={"error":"something wrong"} , status=400)    
